341cda94ad9ca646195d7f8ba6a7475ac834fabd
sourav1547 <souravd2@illinois.edu>
2022-11-28 10:51:23 -0800

    robustfft benchmark

--- benchmark/test_benchmark_qsdh_base.py
+++ benchmark/test_benchmark_qsdh_base.py
@@ -1,14 +1,10 @@
-from adkg.poly_commit_hybrid import PolyCommitHybrid
 from pytest import mark
-from adkg.polynomial import polynomials_over
-from adkg.utils.poly_misc import get_omega
-from adkg.adkg import ADKG
 import asyncio
-import numpy as np
+from adkg.extra_proofs import PoK
+
 import uvloop
 asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
-from pypairing import ZR, G1, G2, blsmultiexp as multiexp, dotprod, blsfft
-
+from pypairing import ZR, G1, G2, blsmultiexp as multiexp, pair
 def get_avss_params(n, G1, G2):
     g, h = G1.rand(b'g'), G1.rand(b'h')
     g2 = G2.rand(b'g')
@@ -45,18 +41,35 @@ async def run_qsdh_base(params):
             # output_g2[i][ii] = output_g2[i-1][ii]**(alpha**ii)
 
 
-async def run_qsdh_base_ver(params):
+async def run_qsdh_base_serial(params):
     g, g2, n, logq, = params
     q = 2**logq
     output_g1 = {0:[g]*q}
     output_g2 = {0:[g2]*2}
     proofs = [None]*(n-1)
     
-    def verify(idx):
+    def verify_prev(idx):
+        cur_g = g
         for node in range(1,idx):
+            pok = PoK(cur_g, ZR, multiexp)
+            if not pok.verify(output_g1[node], proofs[node]):
+                exit()
+            cur_g = output_g1[node]
+            sig1 = ZR.rand()
+            sig2 = ZR.rand()
+            sig1_vec = [sig1**i for i in range(q)]
+            l1 = multiexp(output_g1[node], sig1_vec)
+            r1 = g2*(output_g2[0]**sig2)
+            l2 = g*multiexp(output_g1[node][1:], sig1_vec[1:])
+            r2 = g2*(output_g2[1]**sig2)
+            if not pair(l1,r1) == pair(l2,r2):
+                exit()
 
-            
+       
     for i in range(1, n+1):
+        # Verify previoius updates
+        verify_prev(i)
+        # Update the parameters
         alpha = ZR.rand()
         cur_alpha = alpha
         output_g1[i] = [None]*q
@@ -64,3 +77,23 @@ async def run_qsdh_base_ver(params):
         for ii in range(q):
             output_g1[i][ii] = output_g1[i-1][ii]**(cur_alpha)
             cur_alpha = cur_alpha*alpha
+
+        # Generate proof of knowledge of alpha
+        g = output_g1[i-1][1]
+        h = g**alpha
+        pok = PoK(g, ZR, multiexp)
+        pf = pok.prove(alpha, h)
+        proofs[i] = pf
+        
+async def run_qsdh_base_pipe(params):
+    g, g2, n, logq, = params
+    q = 2**logq
+    output_g1 = {0:[g]*q}
+    output_g2 = {0:[g2]*2}
+    proofs = [None]*(n-1)
+    
+    def verify(idx):
+        cur_g = g
+        for node in range(1,idx):
+            sig_node = ZR.rand()
+            sigma_vec = []
---
+++ benchmark/test_benchmark_robustfft.py
@@ -0,0 +1,96 @@
+from pypairing import ZR, G1, blsfft, blsmultiexp, robustblsfft
+from adkg.polynomial import polynomials_over
+from adkg.utils.poly_misc import prep_for_fft
+from random import shuffle
+from pytest import mark
+import asyncio
+
+def get_omega(field, n, seed=None):
+    """
+    Given a field, this method returns an n^th root of unity.
+    If the seed is not None then this method will return the
+    same n'th root of unity for every run with the same seed
+
+    This only makes sense if n is a power of 2!
+    """
+    assert n & n - 1 == 0, "n must be a power of 2"
+    x = field.rand(seed)
+    y = x**int(field(-1)/n)
+    if y == 1 or y**(n//2) == 1:
+        return get_omega(field, n)
+    assert y**n == 1, "omega must be 2n'th root of unity"
+    assert y**(n // 2) != 1, "omega must be primitive 2n'th root of unity"
+    return y
+
+
+@mark.parametrize("n, t, logq", [(16, 10, 10), (32, 21, 10)])
+def test_benchmark_qsdh_base(test_router, benchmark, n, t, logq):
+    loop = asyncio.get_event_loop()
+    omega2 = get_omega(ZR, 2*n)
+    omega = omega2**2
+    g = G1.rand(b'')
+
+    p = polynomials_over(ZR)
+    q = 2**logq
+    m = q//(t+1)
+    omega = omega2**2
+
+    zs = list(range(n))
+    shuffle(zs)
+    zs = zs[:t+1]
+    polys = [p.random(t) for _ in range(m)]
+    all_ys = [polys[i].evaluate_fft(omega, n) for i in range(m)]
+    ys = []
+    for i in range(m):
+        for j in zs:
+            ys.append(g**all_ys[i][j])
+
+    def _prog():
+        loop.run_until_complete(run_base(zs, ys, omega2, n))
+
+    benchmark(_prog)
+
+async def run_base(zs, ys, omega2, n):
+    robustblsfft(zs, ys, omega2, n)
+
+# @mark.parametrize("n, t, logq", [(16, 10, 10), (16, 10, 15), (32, 21, 10), (32, 21, 15), (64, 42, 10), (64, 42, 15), (128, 85, 10), (32, 85, 15)])
+@mark.parametrize("n, t, logq", [(16, 10, 10), (32, 21, 10)])
+def test_benchmark_qsdh_naive(test_router, benchmark, n, t, logq):
+    loop = asyncio.get_event_loop()
+    omega = get_omega(ZR, n)
+    g = G1.rand(b'')
+
+    p = polynomials_over(ZR)
+    q = 2**logq
+    m = q//(t+1)
+
+    zs = list(range(n))
+    shuffle(zs)
+    zs = zs[:t+1]
+    polys = [p.random(t) for _ in range(m)]
+    all_ys = [polys[i].evaluate_fft(omega, n) for i in range(m)]
+    all_evals = {}
+    for node in zs:
+        all_evals[node] = []
+    
+    for i in range(m):
+        for node in zs:
+            all_evals[node].append(g**all_ys[i][node])
+
+    def _prog():
+        loop.run_until_complete(run_basic_interpolate(all_evals, m, omega, t, n))
+
+    benchmark(_prog)
+
+async def run_basic_interpolate(all_evals, ell, omega, t, n):
+    omegainv = omega**(-1)
+    ninv = ZR(n)**(-1)
+
+    for i in range(ell):
+        coords = [None]*n
+        for node, evals in all_evals.items():
+            coords[node] = evals[i]
+        coords = prep_for_fft(coords, omega, n, blsmultiexp, ZR)
+        
+        fft_inv = blsfft(coords, omegainv, n)
+        [x**ninv for x in fft_inv[:t+1]]
